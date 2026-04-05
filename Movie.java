dbe6e94be4ab64ee2ccbb59fa52573d277d4d288
ekin özer <ekinozer07@gmail.com>
2026-04-05 23:45:19 +0300

    added setter methods

--- Movie.java
+++ Movie.java
@@ -19,10 +19,125 @@ public class Movie {
     protected String movieFunFact;
     protected String movieTrailer; //URL
     
+    private String posterUrl;
+    private User   recommendedBy; // arkadas satiri icin
+
+
     public Movie(String movieTitle, String movieId) {
         this.id = movieId;
         this.title = movieTitle;
     }
 
-    
+
+    public String getId() {
+        return id;
+    }
+ 
+    public String getTitle() {
+        return title;
+    }
+ 
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+ 
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+ 
+    public String getDirector() {
+        return director;
+    }
+ 
+    public void setDirector(String director) {
+        this.director = director;
+    }
+ 
+    public ArrayList<Genre> getGenres() {
+        return genres;
+    }
+ 
+    public void setGenres(ArrayList<Genre> genres) {
+        this.genres = genres;
+    }
+ 
+    public double getAverageRating() {
+        return averageRating;
+    }
+ 
+    public void setAverageRating(double averageRating) {
+        this.averageRating = averageRating;
+    }
+ 
+    public long getNumberOfViews() {
+        return numberOfViews;
+    }
+ 
+    public void setNumberOfViews(long numberOfViews) {
+        this.numberOfViews = numberOfViews;
+    }
+ 
+    public long getNumberOfFavorites() {
+        return numberOfFavorites;
+    }
+ 
+    public void setNumberOfFavorites(long numberOfFavorites) {
+        this.numberOfFavorites = numberOfFavorites;
+    }
+ 
+    public int getDuration() {
+        return duration;
+    }
+ 
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+ 
+    public boolean isNew() {
+        return isNew;
+    }
+ 
+    public void setNew(boolean isNew) {
+        this.isNew = isNew;
+    }
+ 
+    public String getMovieOverview() {
+        return movieOverview;
+    }
+ 
+    public void setMovieOverview(String movieOverview) {
+        this.movieOverview = movieOverview;
+    }
+ 
+    public String getMovieFunFact() {
+        return movieFunFact;
+    }
+ 
+    public void setMovieFunFact(String movieFunFact) {
+        this.movieFunFact = movieFunFact;
+    }
+ 
+    public String getMovieTrailer() {
+        return movieTrailer;
+    }
+ 
+    public void setMovieTrailer(String movieTrailer) {
+        this.movieTrailer = movieTrailer;
+    }
+ 
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+ 
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+ 
+    public User getRecommendedBy() {
+        return recommendedBy;
+    }
+ 
+    public void setRecommendedBy(User recommendedBy) {
+        this.recommendedBy = recommendedBy;
+    }
 }
