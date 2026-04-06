f0f436e7771fc05bc0dc907a77070287fc7efd1e
Ceren Şahin <bcs.ceren@gmail.com>
2026-04-06 22:18:15 +0300

    the search method is now implemented
    search button is working, added the comment lines

--- SearchFriends.java
+++ SearchFriends.java
@@ -1,42 +1,68 @@
 package com.movies;
 
-import javafx.application.Application;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class SearchFriends extends Application{
-    public static void search(String username){}
-    public static void main(String[] args) {
-        launch(args);
+public class SearchFriends{
+    private VBox resultContainer = new VBox();
+
+    public void search(String username, Stage stage) {
+        resultContainer.getChildren().clear();
+        String checkUser = DatabaseHandler.userStringGetter("username", "username", username);
+        if (!checkUser.equals("userstringnotfound")) {
+            Button userButton = new Button(checkUser);
+            userButton.setPrefSize(1200, 100);
+            userButton.setStyle("-fx-background-color: #282B35; -fx-text-fill: #EAEAEA;");
+            userButton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    ProfilePage profilePage = new ProfilePage();
+                    stage.setScene(profilePage.createProfilePageScene(stage));
+                }
+            });
+            
+            resultContainer.getChildren().add(userButton);
+        } else {
+            Label errorLabel = new Label("user not found");
+            errorLabel.setStyle("-fx-text-fill: #FF5555; -fx-font-size: 16px;");
+            resultContainer.getChildren().add(errorLabel);
+        }
     }
-    @Override
-    public void start(Stage arg0) throws Exception {
-        TextField searchFriend= new TextField();
-        String userSearched= searchFriend.getText();
-        searchFriend.setStyle("-fx-background-color: #282B35;"+"-fx-text-fill: #EAEAEA;");
-        Button user1= new Button("mina");
-        user1.setPrefSize(1200, 100);
-        user1.setStyle("-fx-background-color: #282B35;"+"-fx-text-fill: #EAEAEA;");
-        Button user2= new Button("ekin");
-        user2.setPrefSize(1200, 100);
-        user2.setStyle("-fx-background-color: #282B35;"+"-fx-text-fill: #EAEAEA;");
-        Button user3= new Button("bensu");
-        user3.setPrefSize(1200, 100);
-        user3.setStyle("-fx-background-color: #282B35;"+"-fx-text-fill: #EAEAEA;");
-        Button user4= new Button("melisa");
-        user4.setPrefSize(1200, 100);
-        user4.setStyle("-fx-background-color: #282B35;"+"-fx-text-fill: #EAEAEA;");
-        VBox contains= new VBox(50, searchFriend, user1, user2, user3, user4);
+    public Scene choose(Stage primaryStage) {
+        HBox topMenu = new TopMenu().createTopMenu(primaryStage);
+        AnchorPane.setTopAnchor(topMenu, 10.0);
+        AnchorPane.setLeftAnchor(topMenu, 5.0);
+        AnchorPane.setRightAnchor(topMenu, 5.0);
+        TextField searchFriend = new TextField();
+        Button searchButton= new Button("Search");
+        searchButton.setPrefSize(100, 50);
+        searchButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                String userSearched = searchFriend.getText().trim();
+                if (!userSearched.isEmpty()) {
+                    search(userSearched, primaryStage);
+                }
+            }
+        });
+        HBox searchBox= new HBox(searchButton);
+        searchButton.setStyle("-fx-background-color: #282B35;" + "-fx-text-fill: #EAEAEA;");
+        searchBox.setAlignment(Pos.CENTER_LEFT);
+        searchFriend.setStyle("-fx-background-color: #282B35;" + "-fx-text-fill: #EAEAEA;");
+        VBox contains = new VBox(50, topMenu, searchBox, searchFriend);
         contains.setStyle("-fx-background-color: #0B0F1A;");
         contains.setMaxWidth(Double.MAX_VALUE);
-        Scene testScene= new Scene(contains);
-        arg0.setMaximized(true);
-        arg0.setScene(testScene);
-        arg0.show();
+        Scene testScene = new Scene(contains);
+        return testScene;
     }
 }
