77a7d95172537df9d131391a88b2073bb0fd5dab
mina <minaturkyasar@gmail.com>
2026-04-07 20:31:24 +0300

    Update MemberSelectionGroupChat.java
    the profile pictures are not displayed on the screen
    createNewChat() method is not used

--- MemberSelectionGroupChat.java
+++ MemberSelectionGroupChat.java
@@ -25,8 +25,15 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
+
+import java.util.List;
+
 public class MemberSelectionGroupChat {
-    public  Scene createMemberSelectionGroupChatscene(Stage stage) {
+    public Scene createMemberSelectionGroupChatscene(Stage stage, int chatId) {
         AnchorPane root = new AnchorPane();
         HBox topMenu = new TopMenu().createTopMenu(stage);
         AnchorPane.setTopAnchor(topMenu, 10.0);
@@ -42,16 +49,63 @@ public class MemberSelectionGroupChat {
 
         TextField searchField = new TextField();
         searchField.setPromptText("Search for Members...");
+        VBox searchResult = new VBox();
+
+        String username = searchField.getText();
+        // String findingUser = DatabaseHandler.userStringGetter("username", "username",
+        // username);
+
+        // searching logis should be implemented
         AnchorPane.setTopAnchor(searchField, 100.0);
         AnchorPane.setLeftAnchor(searchField, 10.0);
         AnchorPane.setRightAnchor(searchField, 10.0);
-
+        searchField.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                String username = searchField.getText();
+                searchResult.getChildren().clear();
+                if (!username.isEmpty()) {
+                    String foundUsername = DatabaseHandler.userStringGetter("username", "username", username);
+                    if (!foundUsername.equals("userstringnotfound")) { // if found
+                        HBox userRow = new HBox(10);
+                        userRow.setPadding(new Insets(5));
+                        userRow.isAdded(false);
+                        ProfilePhoto userPP = Main.currentUser.getProfilePhoto();
+                        Circle userPPCircle = userPP.ProfilePhoto().createCircle(18);
+                        Label usernameLbl = new Label(foundUsername);
+                        usernameLbl.setStyle("-fx-font-size: 14px;");
+                        Label isAddedLbl = new Label("+");
+                        Region spacer = new Region();
+                        HBox.setHgrow(spacer, Priority.ALWAYS);
+                        userRow.getChildren().addAll(userPPCircle, usernameLbl, spacer, isAddedLbl);
+                        userRow.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                            public void handle(MouseEvent e) {
+                                boolean isAdded = (Boolean) userRow.isAdded();
+                                if (!isAdded) {
+                                    boolean add = DatabaseHandler.addUserToChat(chatId, foundUsername, false);
+                                    if (add) {
+                                        userRow.setUserData(true);
+                                        isAddedLbl.setText("-");
+                                    }
+                                } else {
+                                    userRow.setUserData(false);
+                                    isAddedLbl.setText("+");
+                                }
+                            }
+                        });
+                        searchResult.getChildren().add(userRow);
+                    } else {
+                        Label notFound = new Label("There is no such user");
+                        searchResult.getChildren().add(notFound);
+                    }
+                }
+            }
+        });
         VBox chatList = new VBox(10);
         chatList.setPadding(new Insets(5));
 
         ScrollPane scrollPane = new ScrollPane(chatList);
         scrollPane.setFitToWidth(true);
-        AnchorPane.setTopAnchor(scrollPane, 130.0); 
+        AnchorPane.setTopAnchor(scrollPane, 130.0);
         AnchorPane.setLeftAnchor(scrollPane, 10.0);
         AnchorPane.setRightAnchor(scrollPane, 10.0);
         AnchorPane.setBottomAnchor(scrollPane, 40.0);
@@ -59,34 +113,68 @@ public class MemberSelectionGroupChat {
         Button createGroup = new Button("Create Chat");
         AnchorPane.setRightAnchor(createGroup, 20.0);
         AnchorPane.setBottomAnchor(createGroup, 0.0);
-        createGroup.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-            public void handle(MouseEvent event) {
-                // new scene will be added
-            }
-        });
+        List<HBox> allChatRows = new ArrayList<>();
 
-        //followers and followings will be recommended automatically
-        String ane[] = { "m", "a", "n", "i", "f", "e", "s", "t", "a", "tt" };
-        for (String harf : ane) {
+        // followers and followings will be recommended automatically
+        ArrayList<String> followingList = DatabaseHandler.getFollowings(Main.currentUser.getUsername());
+        ArrayList<String> followersList = DatabaseHandler.getFollowers(Main.currentUser.getUsername());
+        ArrayList<String> allList = new ArrayList<>(followingList);
+        for (String aUser : followersList) {
+            if (!allList.contains(aUser)) {
+                allList.add(aUser);
+            }
+        }
+        for (String recomMember : allList) {
             HBox chatRow = new HBox(20);
             chatRow.setPadding(new Insets(5));
             chatRow.setStyle("-fx-border-color: lightgray; -fx-border-radius: 5; -fx-border-width: 1;");
-            Circle chatPPCircle = new Circle(20, Color.LIGHTBLUE);
-            Label harfs = new Label(harf); // replace with chatname
-            harfs.setPrefWidth(150);
-
-            VBox chatInfoBox = new VBox(3, harfs);
+            ProfilePhoto userPP = Main.currentUser.getProfilePhoto();
+            Circle userPPCircle = userPP.ProfilePhoto().createCircle(18);
+            Label recomName = new Label(recomMember);
+            recomName.setPrefWidth(150);
+            final boolean[] isPlusSign = { true };
+            VBox chatInfoBox = new VBox(3, recomName);
             chatInfoBox.setPrefWidth(200);
             Label isAlreadyAdded = new Label("+");
             VBox joinBox = new VBox(3, isAlreadyAdded);
             joinBox.setAlignment(Pos.CENTER_RIGHT);
+            String thisHarf = recomMember;
+            chatRow.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                public void handle(MouseEvent e) {
+                    if (isPlusSign[0]) {
+                        isAlreadyAdded.setText("-");
+                    } else {
+                        isAlreadyAdded.setText("+");
+                    }
+                    isPlusSign[0] = !isPlusSign[0];
+                }
+            });
             Region spacer = new Region();
             HBox.setHgrow(spacer, Priority.ALWAYS);
-            chatRow.getChildren().addAll(chatPPCircle, chatInfoBox, spacer, joinBox);
+            chatRow.getChildren().addAll(userPPCircle, chatInfoBox, spacer, joinBox);
             chatList.getChildren().add(chatRow);
-        }  
-        contentBox.getChildren().addAll(searchField, scrollPane, chatList,createGroup);
-        root.getChildren().addAll(topMenu, contentBox);  
+            allChatRows.add(chatRow);
+        }
+
+        createGroup.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent e) {
+                for (HBox row : allChatRows) {
+                    Label toggleLabel = (Label) ((VBox) row.getChildren().get(3)).getChildren().get(0);
+                    Label chatNameLabel = (Label) ((VBox) row.getChildren().get(1)).getChildren().get(0);
+                    if ("-".equals(toggleLabel.getText())) {
+                        String chatName = chatNameLabel.getText();
+                        System.out.println(chatName + " added");
+                        //DatabaseHandler.createNewChat()
+                        stage.setScene(new MyChats().createMyChatsScene(stage));
+
+                        // add the members,create the chat, add the chat to mychats page
+                    }
+                }
+            }
+        });
+
+        contentBox.getChildren().addAll(searchField, scrollPane, chatList, createGroup);
+        root.getChildren().addAll(topMenu, contentBox);
 
         return new Scene(root);
     }
