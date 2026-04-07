d37b3379db850b714548d8d442d084057f911288
mina <minaturkyasar@gmail.com>
2026-04-08 00:59:07 +0300

    Update EditProfile.java

--- EditProfile.java
+++ EditProfile.java
@@ -25,9 +25,28 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.Modality;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 public class EditProfile {
-    public  Scene createEditProfileScene(Stage stage) {
+    public Scene createEditProfileScene(Stage stage) {
         AnchorPane root = new AnchorPane();
         HBox topMenu = new TopMenu().createTopMenu(stage);
         AnchorPane.setTopAnchor(topMenu, 10.0);
@@ -45,16 +64,20 @@ public class EditProfile {
         AnchorPane.setTopAnchor(returnToProfile, 0.0);
         AnchorPane.setLeftAnchor(returnToProfile, -15.0);
 
-        Circle userPPCircle = new Circle(27, Color.DODGERBLUE);
-        userPPCircle.setStroke(Color.BLACK);
+        ImageView userPPCircle = new ImageView(new Image(Main.currentUser.getProfilePhoto().getUrl()));
+        userPPCircle.setFitWidth(54);
+        userPPCircle.setFitHeight(54);
+        Circle clip = new Circle(27, 27, 27);
+        userPPCircle.setClip(clip);
+
         AnchorPane.setTopAnchor(userPPCircle, 20.0);
         AnchorPane.setLeftAnchor(userPPCircle, 15.0);
 
-        Label username = new Label("ceren");
+        Label username = new Label(Main.currentUser.getUsername());
         AnchorPane.setTopAnchor(username, 30.0);
         AnchorPane.setLeftAnchor(username, 80.0);
 
-        Label bio = new Label("your average romcom enjoyer");
+        Label bio = new Label(Main.currentUser.getBio());
         AnchorPane.setTopAnchor(bio, 50.0);
         AnchorPane.setLeftAnchor(bio, 80.0);
 
@@ -76,27 +99,53 @@ public class EditProfile {
                         String newBio = editField.getText();
                         bio.setText(newBio);
                         popupStage.close();
-                        
+
                     }
                 });
 
-                newBox.getChildren().addAll(editField, saveBtn );
+                newBox.getChildren().addAll(editField, saveBtn);
                 Scene scene = new Scene(newBox, 300, 200);
                 popupStage.setScene(scene);
 
                 popupStage.show();
             }
         });
-        
 
         Label PPChoosing = new Label("Choose a Profile Photo: ");
         AnchorPane.setTopAnchor(PPChoosing, 100.0);
         AnchorPane.setLeftAnchor(PPChoosing, 40.0);
-        // the options will be added under the label later on
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        int col = 0, row = 0;
+        ArrayList<Button> buttons = new ArrayList<Button>();
+        for (ProfilePhoto photo : ProfilePhoto.values()) {
+            ImageView img = new ImageView(new Image(photo.getUrl()));
+            img.setFitWidth(50);
+            img.setFitHeight(50);
+            Button btn = new Button();
+            btn.setGraphic(img);
+            buttons.add(btn);
+            btn.setOnAction(new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent e) {
+                    Main.currentUser.setProfilePhoto(photo);
+                    for (Button b : buttons) {
+                        b.setDisable(true);
+                    }
+                }
+            });
 
-        Button save = new Button("Save the Changes");
-        AnchorPane.setBottomAnchor(save, 10.0);
-        AnchorPane.setRightAnchor(save, 150.0);
+            grid.add(btn, col, row);
+
+            col++;
+            if (col == 4) {
+                col = 0;
+                row++;
+            }
+        }
+
+        AnchorPane.setTopAnchor(grid, 130.0);
+        AnchorPane.setLeftAnchor(grid, 40.0);
 
         Button deleteAccount = new Button("Delete Account");
         AnchorPane.setBottomAnchor(deleteAccount, 10.0);
@@ -123,7 +172,8 @@ public class EditProfile {
                 deleteBtn.setOnAction(new EventHandler<ActionEvent>() {
                     public void handle(ActionEvent event) {
                         popupStage.close();
-                        // the account should be deleted
+                        DatabaseHandler.deleteUser(Main.currentUser.getUsername());
+                        stage.setScene(new StartingPage().start(stage));
                     }
                 });
 
@@ -136,7 +186,9 @@ public class EditProfile {
             }
         });
 
-        contentBox.getChildren().addAll(returnToProfile, userPPCircle, username, bio, PPChoosing, save, deleteAccount,editBio);
+        contentBox.getChildren().addAll(returnToProfile, userPPCircle, username, bio, PPChoosing, grid,
+                deleteAccount,
+                editBio);
         root.getChildren().addAll(topMenu, contentBox);
 
         return new Scene(root);
