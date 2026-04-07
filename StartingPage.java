ebedb3f3eb3dc6df7e0aaffa1336d70edf77963b
ekin özer <ekinozer07@gmail.com>
2026-04-08 01:50:30 +0300

    Add files via upload

--- StartingPage.java
+++ StartingPage.java
@@ -11,9 +11,7 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class StartingPage extends Application{
-    public static void main(String[] args) {
-        launch(args);
-    }
+    
     private static Stage stage;
     
     @Override
