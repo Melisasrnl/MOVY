0440f08ea48bd3a2ef7497a943329a6de30085bb
ekin özer <ekinozer07@gmail.com>
2026-04-07 20:40:08 +0300

    minor change in login

--- LogIn.java
+++ LogIn.java
@@ -66,28 +66,23 @@ public class LogIn{
 
                 if(DatabaseHandler.isValidateLogin(name, password)){
 
-                    
                     try {
                         MainPage mainPage = new MainPage();
                         Scene nextScene;
                         nextScene = mainPage.createMainPageScene(primaryStage);
                         String email = DatabaseHandler.userStringGetter("email", "username", name);
-                        long id = DatabaseHandler.userIntegerGetter("id", "username",name );
                         
-                        //do not forget to define a public static currentUser in the main/app
-                        Main.currentUser = new User(name, email, id);
+                        
+                        App.currentUser = new User(name, email);
                         
                         String ppURL = DatabaseHandler.userStringGetter("profilepic", "username", name);
 
-                        //i added public static ProfilePhoto fromString(String url) method to ProfilePhoto class
-                        //still not sure about that pp part
-                        //check the new method and consistency
                         if (ppURL != null && !ppURL.equals("userstringnotfound")) {
                             ProfilePhoto profilePhoto = ProfilePhoto.fromString(ppURL);
-                            Main.currentUser.setProfilePhoto(profilePhoto);
+                            App.currentUser.setProfilePhoto(profilePhoto);
                         }
-                        mainPage.setData(Main.currentUser, null);
-                         primaryStage.setScene(nextScene);
+                        mainPage.setData(App.currentUser, null);
+                        primaryStage.setScene(nextScene);
                         primaryStage.setFullScreen(true);
                         primaryStage.show();
                     } catch (Exception e) {
