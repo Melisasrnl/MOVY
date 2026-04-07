70529a7a67ff5913523662d69ee890004d369dd3
ekin özer <ekinozer07@gmail.com>
2026-04-07 20:41:12 +0300

    currentUser is stored in here

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.movies;
+
+import javafx.application.Application;
+
+public class App {
+
+    protected static User currentUser;
+
+    public static void main(String[] args) {
+        Application.launch(StartingPage.class, args);
+    }
+    
+}
