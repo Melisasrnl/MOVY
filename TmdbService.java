3e82c3a7901ac9d76b1bcd7f11a02d996db7042c
Melisa SIRINEL <sirinelmelisa@gmail.com>
2026-04-12 21:41:26 +0300

    Add files via upload

--- TmdbService.java
+++ TmdbService.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class TmdbService {
     
-    private static final String API_KEY = "";
+    
     private static final String BASE_URL = "https://api.themoviedb.org/3";
 
     private static String connectAndGetJson(String endpoint){
